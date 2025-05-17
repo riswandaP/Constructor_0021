a5be59407f51970facbc45e5667322ef098e85bf
riswandaP <riswandaputra03@gmail.com>
2025-05-17 16:51:32 +0700

    Mendefinisikan sebuah fungsi anggota (member function)

--- latihanPertemuan9A.cpp
+++ latihanPertemuan9A.cpp
@@ -86,3 +86,128 @@ public:
     }
 };
 
+void Mahasiswa::lihatInfoDosen(const Dosen& dosen) const {
+    std::cout << "\nInformasi Dosen Pembimbing:" << std::endl;
+    std::cout << "  NIDN: " << dosen.NIDN << std::endl;
+    std::cout << "  Nama: " << dosen.nama << std::endl;
+    std::cout << "  Pangkat: " << dosen.pangkat << std::endl;
+    std::cout << "  Gaji: Rp " << dosen.gaji << std::endl;
+}
+
+class Staff 
+{
+private:
+    std::string IDStaff;
+    std::string nama;
+    double gaji;
+    friend class Universitas; // Universitas bisa mengakses gaji staff
+    friend void lihatGajiStaff(const Staff& staff);
+
+public:
+    Staff(std::string id, std::string nm, double gj) : IDStaff(id), nama(nm), gaji(gj) {}
+
+    double getGaji() const 
+    {
+        return gaji;
+    }
+
+    std::string getNama() const 
+    {
+        return nama;
+    }
+
+    void tampilkanInfoStaff() const 
+    {
+        std::cout << "ID Staff: " << IDStaff << std::endl;
+        std::cout << "Nama Staff: " << nama << std::endl;
+        std::cout << "Gaji: Rp " << gaji << std::endl;
+    }
+};
+
+class Universitas 
+{
+public:
+    void ubahPangkatDosen(Dosen* dosen, std::string pangkatBaru) const 
+    {
+        if (dosen != nullptr) 
+        {
+            dosen->pangkat = pangkatBaru;
+            std::cout << "\nUniversitas: Pangkat dosen " << dosen->getNama() << " berhasil diubah menjadi: " << pangkatBaru << std::endl;
+        } 
+        else 
+        {
+            std::cout << "\nUniversitas: Error - Pointer dosen tidak valid." << std::endl;
+        }
+    }
+
+    void lihatGajiStaff(const Staff& staff) const 
+    {
+        std::cout << "\nUniversitas: Gaji Staff " << staff.getNama() << " adalah: Rp " << staff.getGaji() << std::endl;
+    }
+};
+
+void beriNilai(Mahasiswa& m, double nilai, const Dosen& pengajar) 
+{
+    std::cout << "\nDosen " << pengajar.getNama() << " memberikan nilai " << nilai << " kepada mahasiswa " << m.nama << std::endl;
+    m.nilai = nilai;
+}
+
+void ubahPangkatDosen(Dosen* dosen, std::string pangkatBaru, const Universitas& universitas) 
+{
+    universitas.ubahPangkatDosen(dosen, pangkatBaru);
+}
+
+void lihatGajiDosen(const Dosen& dosen) 
+{
+    std::cout << "\nGaji Dosen (diakses melalui getter): Rp " << dosen.getGaji() << std::endl;
+}
+
+void lihatGajiStaff(const Staff& staff) 
+{
+    std::cout << "\nGaji Staff (diakses melalui getter): Rp " << staff.getGaji() << std::endl;
+}
+
+int main() 
+{
+    // Membuat objek-objek menggunakan pointer
+    Dosen* dosen1 = new Dosen("12345", "Prof. Dr. Budi Santoso", "Guru Besar", 5000000);
+    Mahasiswa* mhs1 = new Mahasiswa("0021", "Siti Aminah", 85.5);
+    Staff* staff1 = new Staff("S001", "Andi Wijaya", 3500000);
+    Universitas* univ1 = new Universitas();
+
+    // Menampilkan informasi awal
+    std::cout << "Informasi Awal:" << std::endl;
+    dosen1->tampilkanInfoDosen();
+    mhs1->tampilkanInfoMahasiswa();
+    staff1->tampilkanInfoStaff();
+
+    // Mahasiswa mengakses informasi dosen melalui friend class
+    mhs1->lihatInfoDosen(*dosen1);
+
+    // Dosen memberikan nilai kepada mahasiswa (menggunakan friend function)
+    beriNilai(*mhs1, 90.0, *dosen1);
+    mhs1->tampilkanInfoMahasiswa();
+
+    // Staff mengubah pangkat dosen melalui friend function dan menggunakan pointer,
+    // yang pada akhirnya memanggil method Universitas
+    ubahPangkatDosen(dosen1, "Lektor Kepala", *univ1);
+    dosen1->tampilkanInfoDosen();
+
+    // Staff melihat gaji dosen melalui friend function
+    lihatGajiDosen(*dosen1);
+
+    // Universitas melihat gaji staff melalui friend function (yang diimplementasikan sebagai method Universitas)
+    univ1->lihatGajiStaff(*staff1);
+
+    // Membersihkan memori yang dialokasikan dengan new
+    delete dosen1;
+    delete mhs1;
+    delete staff1;
+    delete univ1;
+    dosen1 = nullptr;
+    mhs1 = nullptr;
+    staff1 = nullptr;
+    univ1 = nullptr;
+
+    return 0;
+}
