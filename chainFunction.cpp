bea0313be3bb634945c8663c9aed09b4fa9a4a71
riswandaP <riswandaputra03@gmail.com>
2025-05-17 17:45:27 +0700

    Membuat class untuk Buku

--- chainFunction.cpp
+++ chainFunction.cpp
@@ -3,4 +3,20 @@
 #include <string>
 using namespace std;
 
+class Buku 
+{
+private:
+    string judul; 
+
+public:
+    Buku& setJudul(string judul) 
+    { 
+        this->judul = judul;
+        return *this; 
+    }
+    string getJudul() 
+    {
+        return this->judul;
+    }
+} bukunya;
 
