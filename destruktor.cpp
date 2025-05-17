6513c03b4ade48160ad360c8b6e36cc997bd3e90
riswandaP <riswandaputra03@gmail.com>
2025-05-17 17:54:25 +0700

    Mendefinisikan member Function

--- destruktor.cpp
+++ destruktor.cpp
@@ -13,3 +13,18 @@ class angka
         void cetakData();
         void isiData();
 };
+// Definisikan memberr Function
+angka:: angka (int i) // Constructor
+{ 
+    panjang = i;
+    arr = new int[i];
+    isiData();
+}
+
+angka::~angka()
+{ 
+    cout<<endl;
+    cetakData();
+    delete[]arr;
+    cout<<"Alamat Array Sudah Dilepaskan"<<endl;
+}
