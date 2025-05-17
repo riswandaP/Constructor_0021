f85c67647f751d2f81b307160b981c2d01b9e159
riswandaP <riswandaputra03@gmail.com>
2025-05-17 16:44:31 +0700

    Forward declaration dari friend function

--- latihanPertemuan9A.cpp
+++ latihanPertemuan9A.cpp
@@ -8,3 +8,8 @@ class Dosen;
 class Staff;
 class Universitas;
 
+void lihatGajiDosen(const Dosen& dosen);
+void lihatGajiStaff(const Staff& staff);
+void beriNilai(Mahasiswa& m, double nilai, const Dosen& pengajar);
+void ubahPangkatDosen(Dosen* dosen, std::string pangkatBaru, const Universitas& universitas);
+
