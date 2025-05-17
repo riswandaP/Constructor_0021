98cee825f304d46b599cfda8c039d7cfc693ece9
riswandaP <riswandaputra03@gmail.com>
2025-05-17 17:46:00 +0700

    Membuat fungsi int main

--- chainFunction.cpp
+++ chainFunction.cpp
@@ -20,3 +20,10 @@ public:
     }
 } bukunya;
 
+int main()
+{
+    // bukunya.setJudul("Matematika")
+    // cout <<bukunya.getJudul();
+    cout << bukunya.setJudul("Matematika").getJudul(); //chainfunction calls
+    return 0;
+}
