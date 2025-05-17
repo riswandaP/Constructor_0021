737857f62d717368476b7547085b968bdf231ba3
riswandaP <riswandaputra03@gmail.com>
2025-05-17 16:49:02 +0700

    Membuat class untuk Mahasiswa

--- latihanPertemuan9A.cpp
+++ latihanPertemuan9A.cpp
@@ -13,3 +13,40 @@ void lihatGajiStaff(const Staff& staff);
 void beriNilai(Mahasiswa& m, double nilai, const Dosen& pengajar);
 void ubahPangkatDosen(Dosen* dosen, std::string pangkatBaru, const Universitas& universitas);
 
+class Mahasiswa 
+{
+private:
+    std::string NIM;
+    std::string nama;
+    double nilai;
+    friend class Dosen;
+    friend class Universitas;
+    friend void lihatGajiDosen(const Dosen& dosen);
+    friend void beriNilai(Mahasiswa& m, double nilai, const Dosen& pengajar);
+    friend void ubahPangkatDosen(Dosen* dosen, std::string pangkatBaru, const Universitas& universitas);
+
+public:
+    Mahasiswa(std::string nim, std::string nm, double nil) : NIM(nim), nama(nm), nilai(nil) {}
+
+    void tampilkanInfoMahasiswa() const 
+    {
+        std::cout << "NIM: " << NIM << std::endl;
+        std::cout << "Nama Mahasiswa: " << nama << std::endl;
+        std::cout << "Nilai: " << nilai << std::endl;
+    }
+
+    void lihatInfoDosen(const Dosen& dosen) const;
+
+    // Getter untuk nama
+    std::string getNama() const
+    {
+        return nama;
+    }
+
+    // Getter untuk nilai
+    double getNilai() const 
+    {
+        return nilai;
+    }
+
+};
